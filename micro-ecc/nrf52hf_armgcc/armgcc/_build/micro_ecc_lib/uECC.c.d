a5e09335d12903c5582d6dda2cb6e0f789a4ebb2
zhangzheng634712710 <634712710@qq.com>
2019-05-07 19:02:23 +0800

    Add files via upload

---
+++ micro-ecc/nrf52hf_armgcc/armgcc/_build/micro_ecc_lib/uECC.c.d
@@ -0,0 +1,41 @@
+_build/micro_ecc_lib/uECC.c.o: ../../micro-ecc/uECC.c \
+ ../../micro-ecc/uECC.h \
+ /home/zhangzheng/GNUToolsARMEmbedded/8-2018-q4-major/lib/gcc/arm-none-eabi/8.2.1/include/stdint.h \
+ /home/zhangzheng/GNUToolsARMEmbedded/8-2018-q4-major/arm-none-eabi/include/stdint.h \
+ /home/zhangzheng/GNUToolsARMEmbedded/8-2018-q4-major/arm-none-eabi/include/machine/_default_types.h \
+ /home/zhangzheng/GNUToolsARMEmbedded/8-2018-q4-major/arm-none-eabi/include/sys/features.h \
+ /home/zhangzheng/GNUToolsARMEmbedded/8-2018-q4-major/arm-none-eabi/include/_newlib_version.h \
+ /home/zhangzheng/GNUToolsARMEmbedded/8-2018-q4-major/arm-none-eabi/include/sys/_intsup.h \
+ /home/zhangzheng/GNUToolsARMEmbedded/8-2018-q4-major/arm-none-eabi/include/sys/_stdint.h \
+ ../../micro-ecc/uECC_vli.h ../../micro-ecc/types.h \
+ ../../micro-ecc/platform-specific.inc ../../micro-ecc/asm_arm.inc \
+ ../../micro-ecc/asm_arm_mult_square_umaal.inc \
+ ../../micro-ecc/curve-specific.inc
+
+../../micro-ecc/uECC.h:
+
+/home/zhangzheng/GNUToolsARMEmbedded/8-2018-q4-major/lib/gcc/arm-none-eabi/8.2.1/include/stdint.h:
+
+/home/zhangzheng/GNUToolsARMEmbedded/8-2018-q4-major/arm-none-eabi/include/stdint.h:
+
+/home/zhangzheng/GNUToolsARMEmbedded/8-2018-q4-major/arm-none-eabi/include/machine/_default_types.h:
+
+/home/zhangzheng/GNUToolsARMEmbedded/8-2018-q4-major/arm-none-eabi/include/sys/features.h:
+
+/home/zhangzheng/GNUToolsARMEmbedded/8-2018-q4-major/arm-none-eabi/include/_newlib_version.h:
+
+/home/zhangzheng/GNUToolsARMEmbedded/8-2018-q4-major/arm-none-eabi/include/sys/_intsup.h:
+
+/home/zhangzheng/GNUToolsARMEmbedded/8-2018-q4-major/arm-none-eabi/include/sys/_stdint.h:
+
+../../micro-ecc/uECC_vli.h:
+
+../../micro-ecc/types.h:
+
+../../micro-ecc/platform-specific.inc:
+
+../../micro-ecc/asm_arm.inc:
+
+../../micro-ecc/asm_arm_mult_square_umaal.inc:
+
+../../micro-ecc/curve-specific.inc:
